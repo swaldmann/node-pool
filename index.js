7ab839aba22a3cdb521b86cf864a307e6e60ad1a
Steffen Waldmann <steffen.waldmann@sap.com>
2024-08-07 20:38:29 +0200

    Simpler

--- index.js
+++ index.js
@@ -64,18 +64,17 @@ class ResourceRequest {
   }
 }
 
-class WaitingClientsQueue {
+class Queue {
   constructor() {
     this._queue = []
   }
 
-  enqueue(request, priority = 0) {
-    this._queue.push({ request, priority })
-    this._queue.sort((a, b) => b.priority - a.priority)
+  enqueue(request) {
+    this._queue.push(request)
   }
 
   dequeue() {
-    return this._queue.shift()?.request || null
+    return this._queue.shift() || null
   }
 
   get length() {
@@ -83,7 +82,7 @@ class WaitingClientsQueue {
   }
 
   get tail() {
-    return this._queue[this._queue.length - 1]?.request || null
+    return this._queue[this._queue.length - 1] || null
   }
 }
 
@@ -99,23 +98,22 @@ class Pool extends EventEmitter {
       idleTimeoutMillis: 30000,
       acquireTimeoutMillis: null,
       destroyTimeoutMillis: null,
-      maxWaitingClients: null,
       min: 0,
       max: 10
     }, options)
     this._draining = false
     this._started = false
-    this._availableObjects = new Set()
-    this._resourceLoans = new Map()
-    this._allObjects = new Set()
-    this._factoryCreateOperations = new Set()
-    this._waitingClientsQueue = new WaitingClientsQueue()
+    this._available = new Set()
+    this._loans = new Map()
+    this._all = new Set()
+    this._creates = new Set()
+    this._queue = new Queue()
     this.start()
   }
 
   _destroy(resource) {
     resource.updateState(ResourceState.INVALID)
-    this._allObjects.delete(resource)
+    this._all.delete(resource)
     const destroyPromise = this.factory.destroy(resource.obj)
     const wrappedDestroyPromise = this.options.destroyTimeoutMillis ? Promise.race([
           new Promise((_, reject) =>
@@ -125,56 +123,49 @@ class Pool extends EventEmitter {
         ])
       : destroyPromise
     wrappedDestroyPromise.catch(reason => this.emit('factoryDestroyError', reason))
-    this._ensureMinimum()
+    if (this._draining) return
+    for (let i = 0; i < this.options.min - this.size; i++) this._createResource()
   }
 
   _createResource() {
-    const factoryPromise = this.factory.create()
-    this._factoryCreateOperations.add(factoryPromise)
-    factoryPromise.then(resource => {
-        this._factoryCreateOperations.delete(factoryPromise)
+    const _create = this.factory.create()
+    this._creates.add(_create)
+    _create.then(resource => {
+        this._creates.delete(_create)
         const pooledResource = new PooledResource(resource)
-        this._allObjects.add(pooledResource)
+        this._all.add(pooledResource)
         pooledResource.updateState(ResourceState.IDLE)
-        this._availableObjects.add(pooledResource)
+        this._available.add(pooledResource)
         this._dispense()
       })
       .catch(reason => {
-        this._factoryCreateOperations.delete(factoryPromise)
+        this._creates.delete(_create)
         this.emit('factoryCreateError', reason)
         this._dispense()
       })
   }
 
-  _dispatchPooledResourceToNextWaitingClient(resource) {
-    const clientResourceRequest = this._waitingClientsQueue.dequeue()
-    if (!clientResourceRequest) {
-      resource.updateState(ResourceState.IDLE)
-      this._availableObjects.add(resource)
-      return false
-    }
-    this._resourceLoans.set(resource.obj, { pooledResource: resource })
-    resource.updateState(ResourceState.ALLOCATED)
-    clientResourceRequest.resolve(resource.obj)
-    return true
-  }
-
-  _ensureMinimum() {
-    if (this._draining) return
-    const minShortfall = this.options.min - this.size
-    for (let i = 0; i < minShortfall; i++) this._createResource()
-  }
-
   _dispense() {
-    const numWaitingClients = this._waitingClientsQueue.length
-    if (numWaitingClients < 1) return
-    const resourceShortfall = numWaitingClients - (this._availableObjects.size + this._factoryCreateOperations.size)
-    const actualNumberOfResourcesToCreate = Math.min(this.options.max - this.size, resourceShortfall)
-    for (let i = 0; i < actualNumberOfResourcesToCreate; i++) this._createResource()
-    for (let i = 0; i < Math.min(this._availableObjects.size, numWaitingClients); i++) {
-      if (this._availableObjects.size < 1) return false
-      const resource = this._availableObjects.values().next().value
-      this._availableObjects.delete(resource)
+    const waiting = this._queue.length
+    if (waiting < 1) return
+    const shortfall = waiting - (this._available.size + this._creates.size)
+    for (let i = 0; i < Math.min(this.options.max - this.size, shortfall); i++) this._createResource()
+    const _dispatchPooledResourceToNextWaitingClient = resource => {
+      const clientResourceRequest = this._queue.dequeue()
+      if (!clientResourceRequest) {
+        resource.updateState(ResourceState.IDLE)
+        this._available.add(resource)
+        return false
+      }
+      this._loans.set(resource.obj, { pooledResource: resource })
+      resource.updateState(ResourceState.ALLOCATED)
+      clientResourceRequest.resolve(resource.obj)
+      return true
+    }
+    for (let i = 0; i < Math.min(this._available.size, waiting); i++) {
+      if (this._available.size < 1) return false
+      const resource = this._available.values().next().value
+      this._available.delete(resource)
       if (this.options.testOnBorrow) {
         resource.updateState(ResourceState.VALIDATION)
         this.factory.validate(resource.obj)
@@ -185,86 +176,68 @@ class Pool extends EventEmitter {
               this._dispense()
               return
             }
-            this._dispatchPooledResourceToNextWaitingClient(resource)
+            _dispatchPooledResourceToNextWaitingClient(resource)
           })
       } else {
-        if (this._availableObjects.size < 1) return false
-        const resource = this._availableObjects.values().next().value
-        this._availableObjects.delete(resource)
-        this._dispatchPooledResourceToNextWaitingClient(resource)
+        _dispatchPooledResourceToNextWaitingClient(resource)
       }
       return true
     }
   }
 
-  _evict() {
-    const evictionConfig = {
-      softIdleTimeoutMillis: this.options.softIdleTimeoutMillis,
-      idleTimeoutMillis: this.options.idleTimeoutMillis,
-      min: this.options.min
-    }
-    const resourcesToEvict = Array.from(this._availableObjects)
-      .slice(0, this.options.numTestsPerEvictionRun)
-      .filter(resource => {
-        const idleTime = Date.now() - resource.lastIdleTime
-        return (evictionConfig.softIdleTimeoutMillis > 0 && evictionConfig.softIdleTimeoutMillis < idleTime && evictionConfig.min < this._availableObjects.size) ||
-          evictionConfig.idleTimeoutMillis < idleTime
-      })
-
-    resourcesToEvict.forEach(resource => {
-      this._availableObjects.delete(resource)
-      this._destroy(resource)
-    })
-  }
-
   _scheduleEvictorRun() {
     if (this.options.evictionRunIntervalMillis > 0) {
       this._scheduledEviction = setTimeout(() => {
-        this._evict()
+        const evictionConfig = {
+          softIdleTimeoutMillis: this.options.softIdleTimeoutMillis,
+          idleTimeoutMillis: this.options.idleTimeoutMillis,
+          min: this.options.min
+        }
+        const resourcesToEvict = Array.from(this._available)
+          .slice(0, this.options.numTestsPerEvictionRun)
+          .filter(resource => {
+            const idleTime = Date.now() - resource.lastIdleTime
+            const softEvict = evictionConfig.softIdleTimeoutMillis > 0 && evictionConfig.softIdleTimeoutMillis < idleTime && evictionConfig.min < this._available.size
+            return softEvict || evictionConfig.idleTimeoutMillis < idleTime
+          })
+        resourcesToEvict.forEach(resource => {
+          this._available.delete(resource)
+          this._destroy(resource)
+        })
         this._scheduleEvictorRun()
       }, this.options.evictionRunIntervalMillis).unref()
     }
   }
 
   start() {
-    if (this._draining || this._started) return
-    this._started = true
+    if (this._draining) return
     this._scheduleEvictorRun()
-    this._ensureMinimum()
+    for (let i = 0; i < this.options.min - this.size; i++) this._createResource()
   }
 
-  acquire(priority) {
-    if (!this._started && !this.options.autostart) this.start()
+  acquire() {
     if (this._draining) return Promise.reject(new Error('pool is draining and cannot accept work'))
-    if (
-      (this.options.max - this.size) < 1 &&
-      this._availableObjects.size < 1 &&
-      this.options.maxWaitingClients !== undefined &&
-      this._waitingClientsQueue.length >= this.options.maxWaitingClients
-    ) {
-      return Promise.reject(new Error('max waitingClients count exceeded'))
-    }
     const resourceRequest = new ResourceRequest(this.options.acquireTimeoutMillis)
-    this._waitingClientsQueue.enqueue(resourceRequest, priority)
+    this._queue.enqueue(resourceRequest)
     this._dispense()
     return resourceRequest.promise
   }
 
   release(resource) {
-    const loan = this._resourceLoans.get(resource)
+    const loan = this._loans.get(resource)
     if (!loan) return Promise.reject(new Error('Resource not currently part of this pool'))
-    this._resourceLoans.delete(resource)
+    this._loans.delete(resource)
     const pooledResource = loan.pooledResource
     pooledResource.updateState(ResourceState.IDLE)
-    this._availableObjects.add(pooledResource)
+    this._available.add(pooledResource)
     this._dispense()
     return Promise.resolve()
   }
 
   destroy(resource) {
-    const loan = this._resourceLoans.get(resource)
+    const loan = this._loans.get(resource)
     if (!loan) return Promise.reject(new Error('Resource not currently part of this pool'))
-    this._resourceLoans.delete(resource)
+    this._loans.delete(resource)
     const pooledResource = loan.pooledResource
     pooledResource.updateState(ResourceState.INVALID)
     this._destroy(pooledResource)
@@ -274,24 +247,19 @@ class Pool extends EventEmitter {
 
   drain() {
     this._draining = true
-    const allResourceRequestsSettled = this._waitingClientsQueue.length > 0 ? this._waitingClientsQueue.tail.promise : Promise.resolve()
+    const allResourceRequestsSettled = this._queue.length > 0 ? this._queue.tail.promise : Promise.resolve()
     return allResourceRequestsSettled
-      .then(() => Promise.all(Array.from(this._resourceLoans.values()).map(loan => loan.pooledResource.promise)))
+      .then(() => Promise.all(Array.from(this._loans.values()).map(loan => loan.pooledResource.promise)))
       .then(() => clearTimeout(this._scheduledEviction))
   }
 
   async clear() {
-    const reflectedCreatePromises = Array.from(this._factoryCreateOperations)
-    await Promise.all(reflectedCreatePromises)
-    for (const resource of this._availableObjects) {
-      this._destroy(resource)
-    }
-    const reflectedDestroyPromises = Array.from(this._factoryDestroyOperations)
-    return Promise.all(reflectedDestroyPromises)
+    await Promise.all(Array.from(this._creates))
+    for (const resource of this._available) this._destroy(resource)
   }
 
   get size() {
-    return this._allObjects.size + this._factoryCreateOperations.size
+    return this._all.size + this._creates.size
   }
 }
 
